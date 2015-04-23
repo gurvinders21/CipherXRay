fe289e5685b90aa1d94ff2abe5a7910aab7fca42
Gurvinder Singh <gurvinders21@outlook.com>
2015-04-23 13:49:26 +0530

    Transmitter Main UI done

--- CipherXRay/src/Transmitter/Main.java
+++ CipherXRay/src/Transmitter/Main.java
@@ -6,10 +6,19 @@ import java.awt.EventQueue;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
+import javax.swing.JLabel;
+import java.awt.Font;
+import javax.swing.SwingConstants;
+import javax.swing.JTextField;
+import javax.swing.JButton;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+import java.awt.SystemColor;
 
 public class Main extends JFrame {
 
 	private JPanel contentPane;
+	private JTextField textField;
 
 	/**
 	 * Launch the application.
@@ -32,11 +41,52 @@ public class Main extends JFrame {
 	 */
 	public Main() {
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(100, 100, 450, 300);
+		setBounds(100, 100, 700, 500);
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
-		contentPane.setLayout(new BorderLayout(0, 0));
 		setContentPane(contentPane);
+		contentPane.setLayout(null);
+		
+		JLabel titleLabel = new JLabel("Cipher XRay : Exposing Cryptographic Operations");
+		titleLabel.setForeground(SystemColor.textHighlight);
+		titleLabel.setHorizontalAlignment(SwingConstants.CENTER);
+		titleLabel.setFont(new Font("Tahoma", Font.BOLD, 16));
+		titleLabel.setBounds(10, 11, 664, 38);
+		contentPane.add(titleLabel);
+		
+		textField = new JTextField();
+		textField.setFont(new Font("Tahoma", Font.PLAIN, 14));
+		textField.setBounds(338, 162, 189, 38);
+		contentPane.add(textField);
+		textField.setColumns(10);
+		
+		JLabel lblKeyParameter = new JLabel("Key Parameter : ");
+		lblKeyParameter.setHorizontalAlignment(SwingConstants.CENTER);
+		lblKeyParameter.setForeground(SystemColor.textHighlight);
+		lblKeyParameter.setFont(new Font("Tahoma", Font.BOLD | Font.ITALIC, 14));
+		lblKeyParameter.setBounds(171, 162, 133, 35);
+		contentPane.add(lblKeyParameter);
+		
+		JButton btnStartTransmitter = new JButton("Start Transmitter");
+		btnStartTransmitter.setFont(new Font("Tahoma", Font.BOLD, 14));
+		btnStartTransmitter.setBounds(100, 264, 200, 40);
+		contentPane.add(btnStartTransmitter);
+		
+		JButton btnNewButton = new JButton("Key Parameter");
+		btnNewButton.setFont(new Font("Tahoma", Font.BOLD, 14));
+		
+		btnNewButton.setBounds(400, 264, 200, 40);
+		contentPane.add(btnNewButton);
+		
+		JButton btnCryptoOperations = new JButton("Crypto Operations");
+		btnCryptoOperations.setFont(new Font("Tahoma", Font.BOLD, 14));
+		btnCryptoOperations.setBounds(250, 364, 200, 40);
+		contentPane.add(btnCryptoOperations);
+		
+		JLabel lblTransmitter = new JLabel("Transmitter");
+		lblTransmitter.setFont(new Font("Tahoma", Font.BOLD, 14));
+		lblTransmitter.setHorizontalAlignment(SwingConstants.CENTER);
+		lblTransmitter.setBounds(20, 60, 654, 38);
+		contentPane.add(lblTransmitter);
 	}
-
 }
